dd29fb54cdf1dd2e0c6c32902af5977347eaf27d
Muhammad Amir Saeed <amirdev639@gmail.com>
2026-02-17 07:25:25 +0500

    set the product img

--- SaleManagementSys/SaleManagementSys/Controllers/ProductController.cs
+++ SaleManagementSys/SaleManagementSys/Controllers/ProductController.cs
@@ -7,10 +7,12 @@ namespace SaleManagementSys.Controllers
     public class ProductController : Controller
     {
         private readonly IProductService _productService;
+        private readonly IWebHostEnvironment _env;
 
-        public ProductController(IProductService productService)
+        public ProductController(IProductService productService, IWebHostEnvironment env)
         {
             _productService = productService;
+            _env = env;
         }
 
         public async Task<IActionResult> Index()
@@ -30,8 +32,22 @@ namespace SaleManagementSys.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Save(Product product)
+        public async Task<IActionResult> Save(Product product, IFormFile? ImageFile)
         {
+            if (ImageFile != null && ImageFile.Length > 0)
+            {
+                var ext = Path.GetExtension(ImageFile.FileName).ToLowerInvariant();
+                if (string.IsNullOrEmpty(ext) || (ext != ".jpg" && ext != ".jpeg" && ext != ".png" && ext != ".gif" && ext != ".webp"))
+                    ext = ".jpg";
+                var fileName = $"{Guid.NewGuid():N}{ext}";
+                var dir = Path.Combine(_env.WebRootPath, "images", "products");
+                Directory.CreateDirectory(dir);
+                var path = Path.Combine(dir, fileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                    await ImageFile.CopyToAsync(stream);
+                product.ImageUrl = "/images/products/" + fileName;
+            }
+
             if (!ModelState.IsValid)
             {
                 TempData["Message"] = "Please fix the errors below.";
